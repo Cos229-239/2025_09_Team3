8c0c0bac2e58acd3044353ca2a9bdc1ead148f25
jennduke <jenfaulk89@gmail.com>
2025-09-18 19:41:39 -0400

    fixed home:WelcomeScreen()

--- pocketbook/lib/main.dart
+++ pocketbook/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         //colorScheme: ColorScheme.dark(),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomeScreenState(),
+      home: WelcomeScreen(),
     );
   }
 }
